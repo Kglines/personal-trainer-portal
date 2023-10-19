430b00b7db9a993ebc035024d1a0e196df1a4d4d
Keith Glines <keithglines@yahoo.com>
2023-10-19 16:46:51 -0400

    Color theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     colors: {
       primary: 'rgba(0,122,184,1)',
       secondary: 'rgba(79,0,69,1)',
+      black: 'rgb(0, 0, 0)',
+      dark: 'rgba(0, 0, 0, 0.4)',
+      white: 'rgba(255, 255, 255)',
+      grey: 'rgb(155, 155, 155)'
     },
     extend: {},
   },
