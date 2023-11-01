23adb1ca01b5595d237eec0d8368d2c2b4d9e441
Keith Glines <keithglines@yahoo.com>
2023-11-01 07:13:21 -0400

    New Announcement

--- frontend/src/forms/NewAnnouncementForm.js
+++ frontend/src/forms/NewAnnouncementForm.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createAnnouncementThunk } from '../store/announcement';
 
 const NewAnnouncementForm = ({ onClose }) => {
+    const dispatch = useDispatch();
+
+    const [date, setDate] = useState('');
+    const [body, setBody] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const payload = {
+            date,
+            body
+        }
+        
+        dispatch(createAnnouncementThunk(payload))
+            .then(() => onClose())
+            .catch((res) => {
+                if (res.data && res.data.errors) setErrors(res.data.errors);
+            });
+    }
   return (
     <section className='flex flex-col bg-black text-white w-96'>
         <div className="bg-primary w-full rounded-sm">
@@ -8,7 +30,7 @@ const NewAnnouncementForm = ({ onClose }) => {
             <i className="fas fa-times text-2xl text-right p-2 right-0 top-0 absolute hover:cursor-pointer hover:rotate-90 transition ease-in-out" onClick={onClose}></i>
         </div>
         <div className='px-4 py-2 mt-2'>
-            <form className='flex flex-col text-white gap-4'>
+            <form className='flex flex-col text-white gap-4' onSubmit={onSubmit}>
                 <label htmlFor="date" className='text-white'>Date:
                     <input type="date" name="date" id="date" className='bg-dark text-white px-1 mx-2' />
                 </label>
