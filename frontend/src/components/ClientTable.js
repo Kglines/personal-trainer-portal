b1f449110183fdc9fbd4a14491ec509d9cef9e7a
Keith Glines <keithglines@yahoo.com>
2023-11-08 08:11:16 -0500

    Client page with client table

--- frontend/src/components/ClientTable.js
+++ frontend/src/components/ClientTable.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 const ClientTable = () => {
   return (
-    <section>
-        <table>
-            <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Active</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Keith</td>
-                    <td>Glines</td>
-                    <td>YES</td>
-                </tr>
-            </tbody>
-        </table>
+    <section className='w-full mx-auto'>
+      <table className='w-5/6 mx-auto text-lg table-auto'>
+        <thead>
+          <tr className='bg-dark'>
+            <th className='border border-white p-2'>First Name</th>
+            <th className='border border-white p-2'>Last Name</th>
+            <th className='border border-white p-2'>Active</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className='even:bg-dark'>
+            <td className='border border-white p-2'>Keith</td>
+            <td className='border border-white p-2'>Glines</td>
+            <td className='border border-white p-2'>YES</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
-  )
+  );
 }
 
 export default ClientTable
--- frontend/src/pages/Clients.js
+++ frontend/src/pages/Clients.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import ClientTable from '../components/ClientTable'
 
 const Clients = () => {
   return (
-    <section className='text-white w-4/5 mx-auto'>Clients</section>
-  )
+    <section className='text-white w-4/5 mx-auto text-center pt-4 pl-48'>
+      <div>
+        <h1 className='text-4xl'>Clients</h1>
+      </div>
+      <div>
+        <ClientTable />
+      </div>
+    </section>
+  );
 }
 
 export default Clients
