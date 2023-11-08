2356e224d87fc5e53c42d6bb77d3ffc43c44d8cb
Keith Glines <keithglines@yahoo.com>
2023-11-08 08:07:55 -0500

    Client Table

---
+++ frontend/src/components/ClientTable.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const ClientTable = () => {
+  return (
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Active</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Keith</td>
+                    <td>Glines</td>
+                    <td>YES</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+  )
+}
+
+export default ClientTable
