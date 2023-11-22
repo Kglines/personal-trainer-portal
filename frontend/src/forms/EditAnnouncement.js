f2560c32f76c82f4c564b34a231988d443507184
Keith Glines <keithglines@yahoo.com>
2023-11-22 06:51:31 -0500

    Edit Announcement

--- frontend/src/forms/EditAnnouncement.js
+++ frontend/src/forms/EditAnnouncement.js
@@ -1,15 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { editAnnouncementThunk } from '../store/announcement';
+import { useModal } from '../context/Modal';;
 
 const EditAnnouncement = ({ announcement }) => {
   console.log('Edit Announcement ==== ', announcement)
+
+  const dispatch = useDispatch();
+  
+  const { closeModal } = useModal();
+
+  const [date, setDate] = useState(announcement.date);
+  const [body, setBody] = useState(announcement.body);
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = (e) => {
+      e.preventDefault();
+
+      const payload = {
+          date,
+          body
+      }
+      console.log('Announcement Payload ==== ', payload)
+      dispatch(editAnnouncementThunk(payload))
+          .then(() => closeModal())
+          .catch((res) => {
+              if (res.data && res.data.errors) setErrors(res.data.errors);
+          });
+  }
   return (
     <section>
-        <div>
-            <h1>Edit Announcement</h1>
-        </div>
-        <form></form>
+      <div>
+        <h1>Edit Announcement</h1>
+      </div>
+      <form className='flex flex-col text-white gap-4' onSubmit={onSubmit}>
+        <label htmlFor='date' className='text-white'>
+          Date:
+          <input
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            type='date'
+            name='date'
+            id='date'
+            className='bg-dark text-white px-1 mx-2'
+          />
+        </label>
+        <label htmlFor='announcement' className='flex flex-col'>
+          Announcement:
+          <textarea
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            name='announcement'
+            id='announcement'
+            cols='30'
+            rows='10'
+            className='bg-dark py-1 px-2 my-1'
+            placeholder='New Announcement here...'
+          ></textarea>
+        </label>
+        <button
+          type='submit'
+          className='bg-secondary hover:bg-secondaryLight h-8 disabled:bg-dark'
+          disabled={!body || !date}
+        >
+          Add
+        </button>
+      </form>
     </section>
-  )
+  );
 }
 
 export default EditAnnouncement
