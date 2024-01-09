ac0109d33f50b4d66f5f798401ca0135b3046c66
Keith Glines <keithglines@yahoo.com>
2024-01-09 15:49:29 -0500

    Get all comments

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { requireAuth } = require('../../utils/auth');
+
+router.get('/', requireAuth, async (req, res) => {
+    const comments = await Comment.findAll();
+    return res.json(comments);
+})
 
 module.exports = router;
