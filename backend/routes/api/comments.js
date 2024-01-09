47085a2c6cd85259a754fa9103a662e473a05134
Keith Glines <keithglines@yahoo.com>
2024-01-09 15:54:28 -0500

    Comments Store Creation

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { Announcement } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 
+// Get All Announcements
 router.get('/', requireAuth, async (req, res) => {
     const comments = await Comment.findAll();
     return res.json(comments);
 })
+
+
 
 module.exports = router;
---
+++ frontend/src/store/comment.js
@@ -0,0 +1,47 @@
+import { csrfFetch } from './csrf';
+
+// ********************** Action Types ********************** //
+const GET_COMMENTS = 'comments/get';
+const GET_ONE_COMMENT = 'comments/getOne';
+const CREATE_COMMENT = 'comments/create';
+const EDIT_COMMENT = 'comments/edit';
+const DELETE_COMMENT = 'comments/delete';
+
+// ********************** Action Creators ********************** //
+export const getComments = (comments) => {
+  return {
+    type: GET_COMMENTS,
+    payload: comments
+  };
+}
+
+export const getOneComment = (comment) => {
+    return {
+        type: GET_ONE_COMMENT,
+        payload: comment
+    }
+}
+
+export const createComment = (comment) => {  
+  return {
+    type: CREATE_COMMENT,
+    payload: comment
+  };
+}
+
+export const editComment = (comment) => {
+  return {
+    type: EDIT_COMMENT,
+    payload: comment
+  };
+}
+
+export const deleteComment = (commentId) => {
+  return {
+    type: DELETE_COMMENT,
+    payload: commentId
+  };
+}
+
+// // ********************** Thunks ********************** //
+
