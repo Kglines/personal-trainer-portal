696fbbadde9d5993976c8090e4a06e32d990622e
Keith Glines <keithglines@yahoo.com>
2024-01-11 08:17:27 -0500

    Update comment migration

--- backend/db/migrations/20240111130340-create-comment.js
+++ backend/db/migrations/20240111130340-create-comment.js
@@ -41,9 +41,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Comments');
+    options.tableName = 'Comments';
+    await queryInterface.dropTable(options);
   },
 };
