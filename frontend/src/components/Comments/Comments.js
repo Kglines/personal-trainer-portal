535ec1a1e0269f2dc15dbac7ae9c27e452be1b9f
Keith Glines <keithglines@yahoo.com>
2024-01-15 08:37:27 -0500

    Comments layout

---
+++ frontend/src/components/Comments/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Comments = () => {
+  return (
+    <section>
+        <div>
+            <h2>Comments</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Comments
