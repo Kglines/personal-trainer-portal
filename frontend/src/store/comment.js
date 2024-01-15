568fbc895bd4691d83ce7b68214264a895175f1e
Keith Glines <keithglines@yahoo.com>
2024-01-15 08:34:21 -0500

    Redux store for Comments

--- frontend/src/store/comment.js
+++ frontend/src/store/comment.js
@@ -44,7 +44,7 @@ export const deleteComment = (commentId) => {
 }
 
 // // ********************** Thunks ********************** //
-const getCommentsThunk = () => async (dispatch) => {
+const getAllCommentsThunk = () => async (dispatch) => {
     const res = await csrfFetch('/api/comments');
 
     if(res.ok){
@@ -54,3 +54,101 @@ const getCommentsThunk = () => async (dispatch) => {
     }
     return res;
 }
+
+const getAnnouncementCommentsThunk = (announcementId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/announcements/${announcementId}/comments`);
+
+    if(res.ok){
+        const comments = await res.json();
+        dispatch(getComments(comments));
+        return comments
+    }
+    return res;
+}
+
+const getOneCommentThunk = (commentId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/comments/${commentId}`);
+
+    if(res.ok){
+        const comment = await res.json();
+        dispatch(getOneComment(comment));
+        return comment
+    }
+    return res;
+}
+
+const createCommentThunk = (comment) => async (dispatch) => {
+  const res = await csrfFetch(`/api/announcements/${comment.announcementId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify(comment)
+  });
+
+  if(res.ok){
+    const newComment = await res.json();
+    dispatch(createComment(newComment));
+    return newComment;
+  }
+  return res;
+}
+
+const editCommentThunk = (comment) => async (dispatch) => {
+  const res = await csrfFetch(`/api/comments/${comment.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(comment)
+  });
+
+  if(res.ok){
+    const updatedComment = await res.json();
+    dispatch(editComment(updatedComment));
+    return updatedComment;
+  }
+  return res;
+}
+
+const deleteCommentThunk = (commentId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/comments/${commentId}`, {
+    method: 'DELETE'
+  });
+
+  if(res.ok){
+    const deletedComment = await res.json();
+    dispatch(deleteComment(deletedComment));
+    return deletedComment;
+  }
+  return res;
+}
+
+// ********************** Reducer ********************** //
+const initialState = {};
+
+const commentsReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case GET_COMMENTS:
+      newState = {};
+      action.payload.forEach(comment => {
+        newState[comment.id] = comment;
+      });
+      return newState;
+    case GET_ONE_COMMENT:
+      newState = Object.assign({}, state);
+      newState[action.payload.id] = action.payload;
+      return newState;
+    case CREATE_COMMENT:
+      newState = Object.assign({}, state);
+      newState[action.payload.id] = action.payload;
+      return newState;
+    case EDIT_COMMENT:
+      newState = Object.assign({}, state);
+      newState[action.payload.id] = action.payload;
+      return newState;
+    case DELETE_COMMENT:
+      newState = Object.assign({}, state);
+      delete newState[action.payload];
+      return newState;
+    default:
+      return state;
+  }
+}
+
+export default commentsReducer;
