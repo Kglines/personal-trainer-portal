ba1ea9bf11788417f9122505155a1c14b0b8e9f0
Keith Glines <keithglines@yahoo.com>
2024-03-25 21:30:40 -0400

    Updated User API to include Comments, Clients, and Problems

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,85 +1,80 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
-const { User, Comment, Problems, Client } = require('../../db/models');
-const { validateSignup } = require('../../utils/validation.js');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth.js");
+const { User, Comment, Problem, Client } = require("../../db/models");
+const { validateSignup } = require("../../utils/validation.js");
 const router = express.Router();
 
 // ************************* Get Users *************************
-router.get(
-    '/',
-    requireAuth,
-    async (req, res) => {
-        const users = await User.findAll();
-        return res.json(users);
-    }
-)   
+router.get("/", requireAuth, async (req, res) => {
+  const users = await User.findAll();
+  return res.json(users);
+});
 
 // ************************* Get One User *************************
-router.get(
-    '/:id',
-    requireAuth,
-    async (req, res) => {
-        const { id } = req.params;
-        try {
-            const user = await User.findByPk(id, {
-                include: {
-                    model: Comment,
-                    model: Client
-                },
-            
-            });
-            console.log('user === ', user)
-            return res.json(user);
-            
-        } catch (error) {
-            console.log('ERROR === ', error)
-        }
-    }
-)
+router.get("/:id", requireAuth, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByPk(id, {
+      include: [
+        { model: Comment },
+        { model: Problem },
+        { model: Client },
+      ],
+    });
+    console.log("user === ", user);
+    return res.json(user);
+  } catch (error) {
+    console.log("ERROR === ", error);
+  }
+});
 
 // ************************* Sign Up *************************
-router.post(
-    '/',
-    validateSignup,
-    requireAuth,
-    async (req, res) => {
-        const { email, password, username, firstName, lastName, profileImg, role } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({ email, username, firstName, lastName, hashedPassword, profileImg, role });
+router.post("/", validateSignup, requireAuth, async (req, res) => {
+  const { email, password, username, firstName, lastName, profileImg, role } =
+    req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    email,
+    username,
+    firstName,
+    lastName,
+    hashedPassword,
+    profileImg,
+    role,
+  });
 
-        const safeUser = {
-            id: user.id,
-            username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-            profileImg: user.profileImg,
-            role: user.role
-        };
-        
-        await setTokenCookie(res, safeUser);
+  const safeUser = {
+    id: user.id,
+    username: user.username,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    profileImg: user.profileImg,
+    role: user.role,
+  };
 
-        return res.json({
-            user: safeUser
-        })
-    }
-)
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser,
+  });
+});
 
 // ************************* Update User Info *************************
-router.put(
-    '/:id',
-    requireAuth,
-    async (req, res) => {
-        const { id } = req.params;
-        const { email, username, firstName, lastName, profileImg, role} = req.body;
-        const user = await User.findByPk(id);
-        await user.update({ email, username, firstName, lastName, profileImg, role });
+router.put("/:id", requireAuth, async (req, res) => {
+  const { id } = req.params;
+  const { email, username, firstName, lastName, profileImg, role } = req.body;
+  const user = await User.findByPk(id);
+  await user.update({ email, username, firstName, lastName, profileImg, role });
 
-        return res.json({
-            user
-        })
-    }
-)
+  return res.json({
+    user,
+  });
+});
 
 module.exports = router;
--- frontend/src/pages/Trainer.js
+++ frontend/src/pages/Trainer.js
@@ -7,31 +7,63 @@ const Trainer = () => {
   const { trainerId } = useParams();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.users);
-  console.log('user === ', user);
+
+  console.log('user in Trainer Page === ', user);
+
   useEffect(() => {
     dispatch(fetchOneUserThunk(parseInt(trainerId)));
   }, [dispatch, trainerId]);
+
   return (
-    <section className='flex flex-col justify-center text-offWhite mx-auto mt-12 text-center'>
+    <main className='flex flex-col justify-center text-offWhite mx-auto mt-12 text-center'>
       <div>
         <h2>Trainer</h2>
       </div>
-      <div className='flex justify-between mx-auto'>
-        <img
-          src={user?.profileImg}
-          alt={user?.username}
-          className='rounded-full w-64 mx-auto'
-        />
-        <div className='text-left ml-8'>
-          <div className='flex gap-2'>
-            <p>{user?.firstName}</p>
-            <p>{user?.lastName}</p>
+      <section>
+        <div className='flex justify-between mx-auto'>
+          <img
+            src={user?.profileImg}
+            alt={user?.username}
+            className='rounded-full w-64 mx-auto'
+          />
+          <div className='text-left ml-8'>
+            <div className='flex gap-2'>
+              <p>{user?.firstName}</p>
+              <p>{user?.lastName}</p>
+            </div>
+            <p>{user?.email}</p>
+            <p>{user?.username}</p>
           </div>
-          <p>{user?.email}</p>
-          <p>{user?.username}</p>
         </div>
-      </div>
-    </section>
+      </section>
+      <section>
+        <div>
+          <h2>Clients({user.Clients.length})</h2>
+
+        </div>
+        <div>
+          {user?.Clients.map(client => (
+            <div key={client.id} className="flex gap-2">
+                <p>{client.firstName}</p>
+                <p>{client.lastName}</p>
+              </div>
+          ))}
+        </div>
+      </section>
+      <section className="flex flex-col mx-auto">
+        <div>
+          <h2 className="flex">Comments(<p className="">{user.Comments.length}</p>)</h2>
+            
+        </div>
+        <div>
+          {user?.Comments?.map(comment => (
+            <div key={comment?.id} className="flex gap-2">
+                <p>{comment.body}</p>
+              </div>
+          ))}
+        </div>
+      </section>
+    </main>
   );
 };
 
