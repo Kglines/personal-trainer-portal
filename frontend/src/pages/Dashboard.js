0237961c33611d62d342997d061be004db3f9f9b
Keith Glines <keithglines@yahoo.com>
2024-04-08 18:39:14 -0400

    Dashboard importing announcments

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Dashboard = () => {
+  const announcements = useSelector(state => state.announcements)
   return (
-    <div>Dashboard</div>
+    <main>
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+      <div>
+        <div>
+          <h4>Announcements</h4>
+          <div>
+            {announcements.length} announcements this month.
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
